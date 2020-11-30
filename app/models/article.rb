fc2851c5975e5cab14091f679b15e5f3bd9f8bf4
TYTiago <69578313+TYTiago@users.noreply.github.com>
2020-11-30 20:17:28 +0900

    ActiveStorageを導入

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,23 @@
-class Article < ApplicationRecord
+class Article <  ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :genre
+  belongs_to :date_delivery 
+  belongs_to :category 
+  belongs_to :area 
+  belongs_to :delivery 
+  belongs_to :status 
+  
+  
+  with_options presence: true do
+    validates :date_delivery_id 
+    validates :category_id 
+    validates :area_id 
+    validates :delivery_id 
+    validates :status_id 
+  end
 
-  validates :genre_id, numericality: { other_than: 1 }
+  validates :date_delivery_id , numericality: { other_than: 1 }
+  validates :category_id, numericality: { other_than: 1 }
+  validates :area_id, numericality: { other_than: 1 }
+  validates :delivery_id, numericality: { other_than: 1 }
+  validates :status_id, numericality: { other_than: 1 }
 end
