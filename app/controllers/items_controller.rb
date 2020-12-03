eaffdfe4268030918ba71d8e40f04eb9fb54c9c2
TYTiago <69578313+TYTiago@users.noreply.github.com>
2020-12-03 21:04:14 +0900

    商品削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, {only: [:new, :edit]}
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
 
   def index
@@ -39,16 +39,17 @@ if @item.update(item_params)
 end
 
   def destroy
-    
-  end
-
-  def set_item
-    @item = Item.find(params[:id])
+    @item.destroy
+    redirect_to root_path
   end
 
   private
 
   def item_params
     params.require(:item).permit(:title, :price, :description, :category_id, :date_delivery_id, :area_id, :status_id, :delivery_id, :image ).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
